fb575f9ef6007c4f9519940ed04dd279f474226b
Tony-Limaa <125584909+Anthony-Limaa@users.noreply.github.com>
2024-05-18 08:49:04 -0600

    Add files via upload

---
+++ clase-18-05/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.ArbolCadena.ArbolCadenas;
+import org.example.arbolbinario.Arbol;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        Arbol arbol = new Arbol();
+//        arbol.insertar(4);
+//        arbol.insertar(2);
+//        arbol.insertar(1);
+//        arbol.insertar(3);
+//        arbol.insertar(6);
+//        arbol.insertar(5);
+//        arbol.insertar(7);
+
+        ArbolCadenas cadena = new ArbolCadenas();
+        cadena.insertar("farore");
+        cadena.insertar("Impa");
+        cadena.insertar("Navi");
+        cadena.insertar("Epoma");
+        cadena.insertar("Zelda");
+        cadena.insertar("Anhion");
+    }
+}
